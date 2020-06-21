8933a6d60ecac7f2724da153ccc90df5e3dc7baa
ewaldmire <eddie.waldmire@gmail.com>
2020-06-20 23:28:32 -0500

    Fix find template IDs

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -11,23 +11,35 @@ hammer os create \
 --media "CentOS 8 mirror" \
 --provisioning-templates "Kickstart default,Kickstart default finish,Kickstart default PXELinux"
 
-#Need to find OS ID and Template IDs
-hammer template list --search "Kickstart default"
+#Find and set OS and Template IDs
+FNSHID=$(hammer --csv template list --search 'name = "Kickstart default finish"' | grep -v Id | awk -F, {'print $1'})
+PROVID=$(hammer --csv template list --search 'name = "Kickstart default"' | grep -v Id | awk -F, {'print $1'})
+PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux"' | grep -v Id | awk -F, {'print $1'})
+
+# PXEID=$(hammer --csv template list | grep 'Kickstart default PXELinux' | awk -F, {'print $1'})
+# SATID=$(hammer --csv template list | grep 'Satellite Kickstart Default' | awk -F, {'print $1'})
+# for i in $(hammer --csv os list | awk -F, {'print $1'} | grep -vi '^ID')
+#  do
+#    hammer template add-operatingsystem --id="${PXEID}" --operatingsystem-id="${i}"
+#    hammer os set-default-template --id="${i}" --config-template-id="${PXEID}"
+#    hammer os add-config-template --id="${i}" --config-template-id="${SATID}"
+#    hammer os set-default-template --id="${i}" --config-template-id="${SATID}"
+#   done
 
 #Update the Finish template
 hammer os set-default-template \
 --id 10 \
---provisioning-template-id 51
+--provisioning-template-id $FNSHID #Kickstart default finish
 
 #Update the Provisioning template
 hammer os set-default-template \
 --id 10 \
---provisioning-template-id 51 #Kickstart default
+--provisioning-template-id $PROVID #Kickstart default
 
 #Update the PXELinux template
 hammer os set-default-template \
 --id 10 \
---provisioning-template-id 51
+--provisioning-template-id $PXELID #Kickstart default PXELinux
 
 #hammer template add-operatingsystem \
 #--name "Kickstart default" \
