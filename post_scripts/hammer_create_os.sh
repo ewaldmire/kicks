9d91573edd2f36e345cc5be88eac97333bd8dcab
ewaldmire <eddie.waldmire@gmail.com>
2020-06-20 23:36:04 -0500

    Add lookup for OS ID

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -12,6 +12,7 @@ hammer os create \
 --provisioning-templates "Kickstart default,Kickstart default finish,Kickstart default PXELinux"
 
 #Find and set OS and Template IDs
+OSID=$(hammer --csv os list --search 'title = "CentOS 8"' | grep -v Id | awk -F, {'print $1'})
 FNSHID=$(hammer --csv template list --search 'name = "Kickstart default finish"' | grep -v Id | awk -F, {'print $1'})
 PROVID=$(hammer --csv template list --search 'name = "Kickstart default"' | grep -v Id | awk -F, {'print $1'})
 PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux"' | grep -v Id | awk -F, {'print $1'})
@@ -28,17 +29,17 @@ PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux
 
 #Update the Finish template
 hammer os set-default-template \
---id 10 \
+--id $OSID \
 --provisioning-template-id $FNSHID #Kickstart default finish
 
 #Update the Provisioning template
 hammer os set-default-template \
---id 10 \
+--id $OSID \
 --provisioning-template-id $PROVID #Kickstart default
 
 #Update the PXELinux template
 hammer os set-default-template \
---id 10 \
+--id $OSID \
 --provisioning-template-id $PXELID #Kickstart default PXELinux
 
 #hammer template add-operatingsystem \
