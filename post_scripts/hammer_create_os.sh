ed14abf58c0b3324a6ab35a8af0289e882567770
ewaldmire <eddie.waldmire@gmail.com>
2020-06-20 22:28:57 -0500

    Fixes name to CentOS 8 and associates templates

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -1,7 +1,19 @@
 !#/bin/sh
-hammer -u <username> -p <password> os create
+hammer -u <username> -p <password> os create \
 --name CentOS \
---major 9 \
+--major 8 \
 --architectures x86_64 \
 --partition-tables "Kickstart default" \
 --media "CentOS 8 mirror"
+
+hammer -u <username> -p <password> template add-operatingsystem \
+--name "Kickstart default" \
+--operatingsystem "CentOS 8"
+
+hammer -u <username> -p <password> template add-operatingsystem \
+--name "Kickstart default finish" \
+--operatingsystem "CentOS 8"
+
+hammer -u <username> -p <password> template add-operatingsystem \
+--name "Kickstart default PXELinux" \
+--operatingsystem "CentOS 8"
