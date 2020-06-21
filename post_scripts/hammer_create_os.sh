2937ca3f6da5446ca2b02387ad4b4ccdad4f9420
ewaldmire <eddie.waldmire@gmail.com>
2020-06-20 22:36:30 -0500

    Add templates to OS creation

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -4,16 +4,17 @@ hammer -u <username> -p <password> os create \
 --major 8 \
 --architectures x86_64 \
 --partition-tables "Kickstart default" \
---media "CentOS 8 mirror"
+--media "CentOS 8 mirror" \
+--provisioning-templates "Kickstart default,Kickstart default finish,Kickstart default PXELinux"
 
-hammer -u <username> -p <password> template add-operatingsystem \
---name "Kickstart default" \
---operatingsystem "CentOS 8"
+#hammer -u <username> -p <password> template add-operatingsystem \
+#--name "Kickstart default" \
+#--operatingsystem "CentOS 8"
 
-hammer -u <username> -p <password> template add-operatingsystem \
---name "Kickstart default finish" \
---operatingsystem "CentOS 8"
+#hammer -u <username> -p <password> template add-operatingsystem \
+#--name "Kickstart default finish" \
+#--operatingsystem "CentOS 8"
 
-hammer -u <username> -p <password> template add-operatingsystem \
---name "Kickstart default PXELinux" \
---operatingsystem "CentOS 8"
+#hammer -u <username> -p <password> template add-operatingsystem \
+#--name "Kickstart default PXELinux" \
+#--operatingsystem "CentOS 8"
