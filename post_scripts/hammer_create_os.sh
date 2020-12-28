c76dcfcc413452830f43d51ce801c5bfb580b7d7
ewaldmire <eddie.waldmire@gmail.com>
2020-12-28 09:50:59 -0600

    Add CentOS 7 option
    I was previously running Foreman on CentOS 7, so it would automatically add that OS - now I'm running it on CentOS 8 and need the 7 option

--- post_scripts/hammer_create_os.sh
+++ post_scripts/hammer_create_os.sh
@@ -15,6 +15,40 @@ DOMNAME="mynetwork.com"
 SUBNAME="MyNetwork"
 
 ##################
+#CentOS 7        #
+##################
+hammer os create \
+--name CentOS \
+--major 7 \
+--architectures x86_64 \
+--partition-tables "Kickstart default" \
+--media "CentOS 7 mirror" \
+--provisioning-templates "Kickstart default,Kickstart default finish,Kickstart default PXELinux"
+
+#Find and set OS and Template IDs
+OSID=$(hammer --csv os list --search 'title = "CentOS 7"' | grep -v Id | awk -F, {'print $1'})
+FNSHID=$(hammer --csv template list --search 'name = "Kickstart default finish"' | grep -v Id | awk -F, {'print $1'})
+PROVID=$(hammer --csv template list --search 'name = "Kickstart default"' | grep -v Id | awk -F, {'print $1'})
+PXELID=$(hammer --csv template list --search 'name = "Kickstart default PXELinux"' | grep -v Id | awk -F, {'print $1'})
+
+#Update the Finish template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $FNSHID #Kickstart default finish
+
+#Update the Provisioning template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $PROVID #Kickstart default
+
+#Update the PXELinux template
+hammer os set-default-template \
+--id $OSID \
+--provisioning-template-id $PXELID #Kickstart default PXELinux
+
+
+
+##################
 #CentOS 8        #
 ##################
 hammer os create \
